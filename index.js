ee35d7c5c76c525eb795e4d35c8741f61a7dd8f2
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-05-15 21:02:51 +0300

    assignment completed

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    // return tutorial.charAt(0).toUpperCase() + tutorial.slice(1);
+    let words = tutorial.split(' ');
+    let titleCased = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return titleCased.join(' ');
+  });
 }
